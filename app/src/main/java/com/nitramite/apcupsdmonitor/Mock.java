736e9df36b83c2fae07a8a31aa41f8c82d9e56db
bo0tzz <btdewilligen@gmail.com>
2021-06-20 09:21:25 +0200

    Add test case for apcupsd output parsing

--- app/src/main/java/com/nitramite/apcupsdmonitor/Mock.java
+++ app/src/main/java/com/nitramite/apcupsdmonitor/Mock.java
@@ -7,12 +7,7 @@ import java.nio.charset.StandardCharsets;
 /* Provides mock data */
 public class Mock {
 
-    /**
-     * APCUPSD Mock data
-     */
-    public static InputStream ApcupsdMockData() {
-        String input_test =
-            "APC : 001,044,1035\n" +
+    public static final String APCUPSD_MOCK_DATA = "APC : 001,044,1035\n" +
             "DATE : 2021-02-16 19:18:03 +0100\n" +
             "HOSTNAME : proxmox\n" +
             "VERSION : 3.14.14 (31 May 2016) debian\n" +
@@ -57,7 +52,89 @@ public class Mock {
             "NOMOUTV : 230 Volts\n" +
             "FIRMWARE : UPS 08.8 (ID18)\n" +
             "END APC : 2021-02-16 19:18:32 +0100\n";
-            /*
+    public static final String NUT_MOCK_DATA = "battery.charge: 100\n" +
+            "battery.charge.low: 10\n" +
+            "battery.charge.warning: 20\n" +
+            "battery.mfr.date: CPS\n" +
+            "battery.runtime: 1620\n" +
+            "battery.runtime.low: 300\n" +
+            "battery.type: PbAcid\n" +
+            "battery.voltage: 16.0\n" +
+            "battery.voltage.nominal: 24\n" +
+            "device.mfr: CPS\n" +
+            "device.model: CP900EPFCLCD\n" +
+            "device.serial: 000000000000\n" +
+            "device.type: ups\n" +
+            "driver.name: usbhid-ups\n" +
+            "driver.parameter.pollfreq: 30\n" +
+            "driver.parameter.pollinterval: 5\n" +
+            "driver.parameter.port: auto\n" +
+            "driver.version: DSM6-2-25364-191230\n" +
+            "driver.version.data: CyberPower HID 0.3\n" +
+            "driver.version.internal: 0.38\n" +
+            "input.transfer.high: 260\n" +
+            "input.transfer.low: 170\n" +
+            "input.voltage: 228.0\n" +
+            "input.voltage.nominal: 230\n" +
+            "output.voltage: 260.0\n" +
+            "ups.beeper.status: enabled\n" +
+            "ups.delay.shutdown: 20\n" +
+            "ups.delay.start: 30\n" +
+            "ups.load: 24\n" +
+            "ups.mfr: CPS\n" +
+            "ups.model: CP900EPFCLCD\n" +
+            "ups.productid: 0501\n" +
+            "ups.realpower.nominal: 540\n" +
+            "ups.serial: 000000000000\n" +
+            "ups.status: OL\n" +
+            "ups.test.result: No test initiated\n" +
+            "ups.timer.shutdown: -60\n" +
+            "ups.timer.start: -60\n" +
+            "ups.vendorid: 0764\n";
+    public static final String APC_NMC_MOCK_DATA = "E000: Success\n" +
+            "Status of UPS: On Line, No Alarms Present\n" +
+            "Last Transfer: Due to software command or UPS's test control\n" +
+            "Runtime Remaining: 2 hr 0 min\n" +
+            "Battery State Of Charge: 100.0 %\n" +
+            "Output Voltage: 229.3 VAC\n" +
+            "Output Frequency: 50.0 Hz\n" +
+            "Output Watts Percent: 8.0 %\n" +
+            "Input Voltage: 226.0 VAC\n" +
+            "Input Frequency: 50 Hz\n" +
+            "Battery Voltage: 54.8 VDC\n" +
+            "Internal Temperature: 16.2 C, 61.1 F\n" +
+            "Self-Test Result: Passed\n" +
+            "Self-Test Date: 10/25/2020\n" +
+            "Calibration Result: Not Available\n" +
+            "Calibration Date: Unknown\n";
+    public static final String APC_NMC_9630_MOCK_DATA = "E000: Success\n" +
+            "Status of UPS: Online - Green Mode\n" +
+            "Last Transfer: None\n" +
+            "Input Status: Acceptable\n" +
+            "Next Battery Replacement Date: 11/11/2026\n" +
+            "Runtime Remaining: 1 hr 58 min 0 sec\n" +
+            "Battery State Of Charge: 100.0 %\n" +
+            "Output Voltage: 247.6 VAC\n" +
+            "Output Frequency: 50.1 Hz\n" +
+            "Output Watts Percent: 9.1 %\n" +
+            "Output VA Percent: 10.4 %\n" +
+            "Output Current: 0.66 A\n" +
+            "Output Efficiency: 90.0 %\n" +
+            "Output Energy: 4631.618 kWh\n" +
+            "Input Voltage: 247.6 VAC\n" +
+            "Input Frequency: 50.1 Hz\n" +
+            "Battery Voltage: 27.3 VDC\n" +
+            "Battery Temperature: 20.7 C, 69.2 F\n" +
+            "Self-Test Result: Refused via internal operation due to internal fault o\n" +
+            "Self-Test Date: Unknown\n" +
+            "Calibration Result: Unknown\n" +
+            "Calibration Date: Unknown";
+
+    /**
+     * APCUPSD Mock data
+     */
+    public static InputStream ApcupsdMockData() {
+        /*
             "APC      : 001,037,0940\n" +
             "DATE     : 2019-09-20 09:10:34 +0200\n" +
             "HOSTNAME : nebula\n" +
@@ -97,7 +174,7 @@ public class Mock {
             "FIRMWARE : 925.T2 .I USB FW:T2\n" +
             "END APC  : 2019-09-20 09:10:35 +0200\n";
              */
-        return new ByteArrayInputStream(input_test.getBytes(StandardCharsets.UTF_8));
+        return new ByteArrayInputStream(APCUPSD_MOCK_DATA.getBytes(StandardCharsets.UTF_8));
     }
 
     /**
@@ -105,101 +182,21 @@ public class Mock {
      * command: upsc ups
      */
     public static InputStream SynologyMockData() {
-        String input_test =
-            "battery.charge: 100\n" +
-            "battery.charge.low: 10\n" +
-            "battery.charge.warning: 20\n" +
-            "battery.mfr.date: CPS\n" +
-            "battery.runtime: 1620\n" +
-            "battery.runtime.low: 300\n" +
-            "battery.type: PbAcid\n" +
-            "battery.voltage: 16.0\n" +
-            "battery.voltage.nominal: 24\n" +
-            "device.mfr: CPS\n" +
-            "device.model: CP900EPFCLCD\n" +
-            "device.serial: 000000000000\n" +
-            "device.type: ups\n" +
-            "driver.name: usbhid-ups\n" +
-            "driver.parameter.pollfreq: 30\n" +
-            "driver.parameter.pollinterval: 5\n" +
-            "driver.parameter.port: auto\n" +
-            "driver.version: DSM6-2-25364-191230\n" +
-            "driver.version.data: CyberPower HID 0.3\n" +
-            "driver.version.internal: 0.38\n" +
-            "input.transfer.high: 260\n" +
-            "input.transfer.low: 170\n" +
-            "input.voltage: 228.0\n" +
-            "input.voltage.nominal: 230\n" +
-            "output.voltage: 260.0\n" +
-            "ups.beeper.status: enabled\n" +
-            "ups.delay.shutdown: 20\n" +
-            "ups.delay.start: 30\n" +
-            "ups.load: 24\n" +
-            "ups.mfr: CPS\n" +
-            "ups.model: CP900EPFCLCD\n" +
-            "ups.productid: 0501\n" +
-            "ups.realpower.nominal: 540\n" +
-            "ups.serial: 000000000000\n" +
-            "ups.status: OL\n" +
-            "ups.test.result: No test initiated\n" +
-            "ups.timer.shutdown: -60\n" +
-            "ups.timer.start: -60\n" +
-            "ups.vendorid: 0764\n";
-        return new ByteArrayInputStream(input_test.getBytes(StandardCharsets.UTF_8));
+        return new ByteArrayInputStream(NUT_MOCK_DATA.getBytes(StandardCharsets.UTF_8));
     }
 
     /**
      * APC Network Card Mock data
      */
     public static InputStream APCNetworkCardMockData() {
-        String input_test =
-            "E000: Success\n" +
-            "Status of UPS: On Line, No Alarms Present\n" +
-            "Last Transfer: Due to software command or UPS's test control\n" +
-            "Runtime Remaining: 2 hr 0 min\n" +
-            "Battery State Of Charge: 100.0 %\n" +
-            "Output Voltage: 229.3 VAC\n" +
-            "Output Frequency: 50.0 Hz\n" +
-            "Output Watts Percent: 8.0 %\n" +
-            "Input Voltage: 226.0 VAC\n" +
-            "Input Frequency: 50 Hz\n" +
-            "Battery Voltage: 54.8 VDC\n" +
-            "Internal Temperature: 16.2 C, 61.1 F\n" +
-            "Self-Test Result: Passed\n" +
-            "Self-Test Date: 10/25/2020\n" +
-            "Calibration Result: Not Available\n" +
-            "Calibration Date: Unknown\n";
-        return new ByteArrayInputStream(input_test.getBytes(StandardCharsets.UTF_8));
+        return new ByteArrayInputStream(APC_NMC_MOCK_DATA.getBytes(StandardCharsets.UTF_8));
     }
 
     /**
      * APC Network Card Mock data #2
      */
     public static InputStream APCNetworkCardMockDataAP9630() {
-        String input_test =
-            "E000: Success\n" +
-            "Status of UPS: Online - Green Mode\n" +
-            "Last Transfer: None\n" +
-            "Input Status: Acceptable\n" +
-            "Next Battery Replacement Date: 11/11/2026\n" +
-            "Runtime Remaining: 1 hr 58 min 0 sec\n" +
-            "Battery State Of Charge: 100.0 %\n" +
-            "Output Voltage: 247.6 VAC\n" +
-            "Output Frequency: 50.1 Hz\n" +
-            "Output Watts Percent: 9.1 %\n" +
-            "Output VA Percent: 10.4 %\n" +
-            "Output Current: 0.66 A\n" +
-            "Output Efficiency: 90.0 %\n" +
-            "Output Energy: 4631.618 kWh\n" +
-            "Input Voltage: 247.6 VAC\n" +
-            "Input Frequency: 50.1 Hz\n" +
-            "Battery Voltage: 27.3 VDC\n" +
-            "Battery Temperature: 20.7 C, 69.2 F\n" +
-            "Self-Test Result: Refused via internal operation due to internal fault o\n" +
-            "Self-Test Date: Unknown\n" +
-            "Calibration Result: Unknown\n" +
-            "Calibration Date: Unknown";
-        return new ByteArrayInputStream(input_test.getBytes(StandardCharsets.UTF_8));
+        return new ByteArrayInputStream(APC_NMC_9630_MOCK_DATA.getBytes(StandardCharsets.UTF_8));
     }
 
 
--- app/src/test/java/com/nitramite/apcupsdmonitor/UPSTest.java
+++ app/src/test/java/com/nitramite/apcupsdmonitor/UPSTest.java
@@ -1,12 +1,54 @@
 package com.nitramite.apcupsdmonitor;
 
 
+import android.content.Context;
+
 import junit.framework.TestCase;
 
-public class UPSTest extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
-    public void testApcUpsDStatusParse() {
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UPSTest extends TestCase {
+    @org.mockito.Mock
+    private Context mockContext;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public void testApcUpsdStatusParse() {
+        when(this.mockContext.getString(R.string.ups_start_time)).thenReturn("Start time");
+        when(this.mockContext.getString(R.string.ups_volts_line_voltage)).thenReturn("Volts line voltage");
+        when(this.mockContext.getString(R.string.ups_of_load)).thenReturn("of load");
+        when(this.mockContext.getString(R.string.ups_battery_charge)).thenReturn("battery charge");
+        when(this.mockContext.getString(R.string.ups_battery_time_left)).thenReturn("Battery time left");
+        when(this.mockContext.getString(R.string.ups_last)).thenReturn("Last");
+        when(this.mockContext.getString(R.string.ups_battery_date)).thenReturn("Battery date");
+
         UPS ups = new UPS();
-        assertTrue(true);
+        ups.setUPS_STATUS_STR(Mock.APCUPSD_MOCK_DATA);
+        assertEquals("APCUPS", ups.getUPS_NAME());
+        assertEquals("Start time: 2021-02-16 15:06:17 +0100", ups.getSTART_TIME(this.mockContext));
+        assertEquals("Smart-UPS 1000", ups.getMODEL());
+        assertEquals("UPS ONLINE", ups.getSTATUS());
+        assertEquals("233.0V", ups.getLineVoltageOnlyStr(this.mockContext));
+        assertEquals("18.0 Percent of load", ups.getLoadPercentStr(this.mockContext));
+        assertEquals("100.0 Percent battery charge", ups.getBatteryChargeLevelStr(this.mockContext));
+        assertEquals("Battery time left: 47.0 Minutes", ups.getBATTERY_TIME_LEFT(this.mockContext));
+        assertEquals("40 Percent", ups.getMINIMUM_BATTERY_CHARGE());
+        assertEquals("5 Minutes", ups.getMINIMUM_TIME_LEFT());
+        assertEquals("27.0V", ups.getBatteryVoltageOnlyStr(this.mockContext));
+        assertEquals("Last: Automatic or explicit self test", ups.getLastTransferReasonStr(this.mockContext));
+        assertEquals("0 Seconds", ups.getLAST_SECONDS_ON_BATTERY());
+        assertEquals("N/A", ups.getLAST_TIME_DATE_ON_BATTERY());
+        assertEquals("Battery date 08/15/2014", ups.getBATTERY_DATE(this.mockContext));
+        assertEquals("UPS 08.8 (ID18)", ups.getFIRMWARE());
+        assertEquals("30.0 C", ups.getITEMP());
     }
 }
