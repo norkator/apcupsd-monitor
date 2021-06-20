df663ed4651b4fde70055227c89dfe38df26dc6c
bo0tzz <btdewilligen@gmail.com>
2021-06-20 08:07:48 +0200

    Set up for running unit tests

---
+++ app/src/test/java/com/nitramite/apcupsdmonitor/UPSTest.java
@@ -0,0 +1,12 @@
+package com.nitramite.apcupsdmonitor;
+
+
+import junit.framework.TestCase;
+
+public class UPSTest extends TestCase {
+
+    public void testApcUpsDStatusParse() {
+        UPS ups = new UPS();
+        assertTrue(true);
+    }
+}
