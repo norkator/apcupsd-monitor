ceabd43cc09bb0aa0625f8e83858a347af12881c
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-09-06 19:52:11 +0300

    Getting challenge works

--- app/src/main/java/com/nitramite/apcupsdmonitor/IPM.java
+++ app/src/main/java/com/nitramite/apcupsdmonitor/IPM.java
@@ -3,9 +3,13 @@ package com.nitramite.apcupsdmonitor;
 import android.annotation.SuppressLint;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Objects;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
@@ -27,28 +31,43 @@ public class IPM {
     private final OkHttpClient client = getUnsafeOkHttpClient();
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-
+    
     IPM(String baseUrl, String port) {
         try {
             String challenge = getChallenge(baseUrl, port);
             Log.i(TAG, challenge);
-        } catch (IOException e) {
+        } catch (IOException | JSONException e) {
             Log.e(TAG, e.toString());
             e.printStackTrace();
         }
     }
 
 
-    private String getChallenge(String baseUrl, String port) throws IOException {
+    /**
+     * Returns challenge from IPM required for login request
+     *
+     * @param baseUrl of IPM server
+     * @param port    of IPM server
+     * @return challenge, example a6e3d6d65b072af181dd40658f65051ace864235
+     * @throws IOException   if http call fails
+     * @throws JSONException if parsing challenge fails
+     */
+    private String getChallenge(String baseUrl, String port) throws IOException, JSONException {
         Request request = new Request.Builder()
-                .url(baseUrl + ":" + port + "/server/user_srv.js?action=queryLoginChallenge")
+                .url("https://" + baseUrl + ":" + port + "/server/user_srv.js?action=queryLoginChallenge")
                 .build();
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            JSONObject jsonObject = new JSONObject(Objects.requireNonNull(response.body()).string());
+            return jsonObject.optString("challenge");
         }
     }
 
 
+    /**
+     * Unsecure okHttp client to make request to local network IPM server without valid cert
+     *
+     * @return okHttp instance
+     */
     private static OkHttpClient getUnsafeOkHttpClient() {
         try {
             final TrustManager[] trustAllCerts = new TrustManager[]{
