2e974322f6f0cc8fbfcedfef66b95ddad0e11eca
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-11-15 19:33:12 +0200

    new httpHttpsOptionsLayout at ups editor

--- app/src/main/java/com/nitramite/apcupsdmonitor/UpsEditor.java
+++ app/src/main/java/com/nitramite/apcupsdmonitor/UpsEditor.java
@@ -49,7 +49,7 @@ public class UpsEditor extends AppCompatActivity {
 
     // View elements
     private EditText privateKeyLocationET;
-    private LinearLayout credentialOptionsLayout, sshOptionsLayout, ipmOptionsLayout;
+    private LinearLayout credentialOptionsLayout, sshOptionsLayout, ipmOptionsLayout, httpHttpsOptionsLayout;
     private RadioButton sshRB;
     private RadioButton nisRB;
     private RadioButton ipmRB;
@@ -88,6 +88,7 @@ public class UpsEditor extends AppCompatActivity {
         final EditText eventsLocationET = findViewById(R.id.eventsLocationET);
         final EditText nodeIdET = findViewById(R.id.nodeIdET);
         final Switch upsEnabledSwitch = findViewById(R.id.upsEnabledSwitch);
+        final Switch httpsEnabledSwitch = findViewById(R.id.httpsEnabledSwitch);
 
 
         final Spinner cmdPresetSelection = findViewById(R.id.cmdPresetSelection);
@@ -143,11 +144,13 @@ public class UpsEditor extends AppCompatActivity {
         sshOptionsLayout = findViewById(R.id.sshOptionsLayout);
         credentialOptionsLayout = findViewById(R.id.credentialOptionsLayout);
         ipmOptionsLayout = findViewById(R.id.ipmOptionsLayout);
+        httpHttpsOptionsLayout = findViewById(R.id.httpHttpsOptionsLayout);
 
         sshRB.setOnClickListener(v -> {
             sshOptionsLayout.setVisibility(View.VISIBLE);
             credentialOptionsLayout.setVisibility(View.VISIBLE);
             ipmOptionsLayout.setVisibility(View.GONE);
+            httpHttpsOptionsLayout.setVisibility(View.GONE);
             nisRB.setChecked(false);
             ipmRB.setChecked(false);
         });
@@ -155,6 +158,7 @@ public class UpsEditor extends AppCompatActivity {
             sshOptionsLayout.setVisibility(View.GONE);
             credentialOptionsLayout.setVisibility(View.GONE);
             ipmOptionsLayout.setVisibility(View.GONE);
+            httpHttpsOptionsLayout.setVisibility(View.GONE);
             sshRB.setChecked(false);
             ipmRB.setChecked(false);
         });
@@ -162,6 +166,7 @@ public class UpsEditor extends AppCompatActivity {
             sshOptionsLayout.setVisibility(View.GONE);
             credentialOptionsLayout.setVisibility(View.VISIBLE);
             ipmOptionsLayout.setVisibility(View.VISIBLE);
+            httpHttpsOptionsLayout.setVisibility(View.VISIBLE);
             nisRB.setChecked(false);
             sshRB.setChecked(false);
         });
@@ -222,6 +227,7 @@ public class UpsEditor extends AppCompatActivity {
             loadUpsEventsSwitch.setChecked(ups.getUpsLoadEvents());
             nodeIdET.setText(ups.UPS_NODE_ID);
             upsEnabledSwitch.setChecked(ups.UPS_ENABLED);
+            httpsEnabledSwitch.setChecked(ups.UPS_USE_HTTPS);
         }
 
 
@@ -264,6 +270,7 @@ public class UpsEditor extends AppCompatActivity {
             contentValues.put(DatabaseHelper.UPS_LOAD_EVENTS, loadUpsEventsSwitch.isChecked() ? "1" : "0");
             contentValues.put(DatabaseHelper.UPS_NODE_ID, nodeIdET.getText().toString());
             contentValues.put(DatabaseHelper.UPS_ENABLED, upsEnabledSwitch.isChecked() ? 1 : 0);
+            contentValues.put(DatabaseHelper.UPS_USE_HTTPS, httpsEnabledSwitch.isChecked() ? 1 : 0);
             databaseHelper.insertUpdateUps(null, upsId, contentValues);
             Toast.makeText(UpsEditor.this, R.string.saved, Toast.LENGTH_SHORT).show();
             UpsEditor.this.finish();
