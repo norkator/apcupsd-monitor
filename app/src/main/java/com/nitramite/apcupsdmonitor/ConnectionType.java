8267a095a51687fd2c6aeec705ecb60d4c17ab6d
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-01-22 17:45:33 +0200

    new connection type NUT

--- app/src/main/java/com/nitramite/apcupsdmonitor/ConnectionType.java
+++ app/src/main/java/com/nitramite/apcupsdmonitor/ConnectionType.java
@@ -7,5 +7,6 @@ public class ConnectionType {
     public static final String UPS_CONNECTION_TYPE_SSH = "0"; // SSH (Secure Shell)
     public static final String UPS_CONNECTION_TYPE_NIS = "1"; // APCUPSD (Network Information Server)
     public static final String UPS_CONNECTION_TYPE_IPM = "2"; // Eaton IPM (Intelligent Power Manager)
+    public static final String UPS_CONNECTION_TYPE_NUT = "3"; // NUT server (Network UPS Tools)
 
 }
