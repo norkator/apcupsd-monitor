cfd271fe3fcfccbcad4fa31051ff4812f5cbee31
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2024-07-21 15:37:03 +0300

    add display_name field to sqlite db

--- app/src/main/java/com/nitramite/apcupsdmonitor/UPS.java
+++ app/src/main/java/com/nitramite/apcupsdmonitor/UPS.java
@@ -35,6 +35,7 @@ public class UPS {
     public String UPS_NODE_ID = null; // IPM this is serial number of your device
     public boolean UPS_ENABLED = true; // cab disable ups momentarily if user wants so
     public boolean UPS_USE_HTTPS = true;
+    public String UPS_DISPLAY_NAME;
 
     // Variables | status and event strings
     private String UPS_STATUS_STR = null;
